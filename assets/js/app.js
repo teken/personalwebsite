4e133407122e430f9d4dcf667a145cfeac71bdfd
Anna <duncanhaig@btopenworld.com>
2019-02-27 01:25:41 +0000

    back to home added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-    $('.clickable .links > a').on('click', (event) => {
+    $('.clickable .links > a').filter((i, item) => !$(item).hasClass('ignore')).on('click', (event) => {
         event.preventDefault();
         const href = event.currentTarget.href;
         const clean = href.slice(href.lastIndexOf('#')+1);
